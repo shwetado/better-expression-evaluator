3aaf892c0232d52ab651fb1084bf1e0815702b3a
Shweta Dongare <shwetadongare28@gmail.com>
2014-02-08 10:50:50 +0530

    Delete Subtract.java

--- src/shwetado/evaluator/Subtract.java
+++
@@ -1,8 +0,0 @@
-package shwetado.evaluator;
-
-public class Subtract implements Operator{
-    @Override
-    public double operate(Expression right, Expression left) {
-        return right.evaluate() - left.evaluate();
-    }
-}
