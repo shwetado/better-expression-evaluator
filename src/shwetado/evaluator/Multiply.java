2c47e93285c92ce2cf10fa14c5c0e3ad67284aa8
Shweta Dongare <shwetadongare28@gmail.com>
2014-02-08 10:50:30 +0530

    Delete Multiply.java

--- src/shwetado/evaluator/Multiply.java
+++
@@ -1,8 +0,0 @@
-package shwetado.evaluator;
-
-public class Multiply implements Operator{
-    @Override
-    public double operate(Expression right, Expression left) {
-        return right.evaluate() * left.evaluate();
-    }
-}
