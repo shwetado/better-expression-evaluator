07f52c0fe3551f5cd8adc671918072dddfc3e9dc
Shweta Dongare <shwetadongare28@gmail.com>
2014-02-08 10:50:02 +0530

    Delete Division.java

--- src/shwetado/evaluator/Division.java
+++
@@ -1,8 +0,0 @@
-package shwetado.evaluator;
-
-public class Division implements Operator{
-    @Override
-    public double operate(Expression right, Expression left) {
-        return right.evaluate() / left.evaluate();
-    }
-}
